f3bd74913c5a96ff4cb413999c89e8d6a118e3b6
myckhel <myckhel1@hotmail.com>
2021-08-05 02:15:39 +0100

    added route config

--- config/mono.php
+++ config/mono.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    "secret_key"    => env("MONO_SECRET_KEY", 'yes'),
+    "secret_key"    => env("MONO_SECRET_KEY"),
     "url"           => env("MONO_URL", 'https://api.withmono.com'),
 
-    "routes" => [
-        "middleware"    => null, // For injecting middleware to the package's routes
+    "route" => [
+        "middleware"    => ['api'], // For injecting middleware to the package's routes
+        "prefix"    => 'api', // For injecting middleware to the package's routes
     ],
 ];
