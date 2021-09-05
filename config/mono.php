832bbc1117858f5c806caf3a7881f27a20c7043f
myckhel <myckhel1@hotmail.com>
2021-09-05 02:35:57 +0100

    added `webhook_secret` to config

--- config/mono.php
+++ config/mono.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    "secret_key"    => env("MONO_SECRET_KEY"),
-    "url"           => env("MONO_URL", 'https://api.withmono.com'),
-    "version"       => 1,
+    "secret_key"        => env("MONO_SECRET_KEY"),
+    "webhook_secret"    => env("MONO_WEBHOOK_SECRET_KEY"),
+    "url"               => env("MONO_URL", 'https://api.withmono.com'),
+    "version"           => 1,
 
-    "route" => [
+    "route"             => [
         "middleware"    => ['api'], // For injecting middleware to the package's routes
-        "prefix"    => 'api', // For injecting middleware to the package's routes
+        "prefix"        => 'api', // For injecting middleware to the package's routes
     ],
 ];
